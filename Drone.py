84330643691b1a063cc32ee8eeee40d336282fbb
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2020-02-19 15:24:43 +0100

    Added ground force and unknown force calculations

--- Drone.py
+++ Drone.py
@@ -1,4 +1,6 @@
-from pandas import DataFrame
+from pandas import DataFrame, Series
+from sklearn.linear_model import LinearRegression
+import numpy as np
 
 from Measurement import read_measurements
 from PolynomialRegression import PolynomialRegression
@@ -18,14 +20,42 @@ class Drone:
 
 
 def find_f0(df: DataFrame):
-    polynomial_regression = PolynomialRegression(3, bias=False)
+    polynomial_regression = PolynomialRegression(3, bias=True)
     average_voltages = df.filter(regex="^voltage_\\d+$").mean(axis=1)
     average_weights = df.filter(regex="^weight_\\d+$").mean(axis=1)
     polynomial_regression.fit(average_voltages.values, average_weights.values)
     return polynomial_regression
 
 
+def find_fge(df: DataFrame, f0):
+    polynomial_regression = PolynomialRegression(3, bias=True)
+    average_voltages = df.filter(regex="^voltage_\\d+$").mean(axis=1)
+    calc_weights = f0.predict(average_voltages.values)
+    average_weights = df.filter(regex="^weight_\\d+$").mean(axis=1)
+    error_weights = average_weights / calc_weights
+    distances = df["distance"]
+    polynomial_regression.fit(distances.values, error_weights.values)
+    return polynomial_regression
+
+
+def find_fu(df: DataFrame, f0, fge):
+    df = df.drop(["time"], axis=1)
+    lr = LinearRegression()  # Most basic model
+    voltages = df.filter(regex="^voltage_\\d+$")
+    average_voltages = voltages.mean(axis=1)
+    weights = df.filter(regex="^weight_\\d+$")
+    distances = df["distance"]
+    pred_weights = f0.predict(average_voltages.values) * fge.predict(distances.values)
+    error_weights = weights.values - np.repeat(np.reshape(pred_weights, (-1, 1)), 4, axis=1)
+    lr.fit(df.values, error_weights)
+    return lr
+
+
 if __name__ == '__main__':
     df = read_measurements("example_measurements.csv")
     f0 = find_f0(df[df["distance"] >= 20])
+    fge = find_fge(df, f0)
+    fu = find_fu(df, f0, fge)
     print(f0)
+    print(fge)
+    print(fu)
--- Measurement.py
+++ Measurement.py
@@ -6,7 +6,7 @@ class MeasurementWriter:
     def __init__(self, fname, n_measurements=4):
         self.file = open(fname, "w")
         self.n_measurements = n_measurements
-        columns = ["time", "dist"] + \
+        columns = ["time", "distance"] + \
                   [f"voltage_{n}" for n in range(n_measurements)] + \
                   [f"weight_{n}" for n in range(n_measurements)]
         self.file.write(",".join(columns) + "\n")
