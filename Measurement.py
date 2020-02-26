c619de54e8614a1b9a11e10247eb8d9a5cb35f0d
Rolv-Arild Braaten <rolv_arild@hotmail.com>
2020-02-26 19:50:45 +0100

    Added get_forces method

--- Measurement.py
+++ Measurement.py
@@ -1,5 +1,6 @@
 import time
 import pandas as pd
+import numpy as np
 
 
 class MeasurementWriter:
@@ -23,7 +24,43 @@ def read_measurements(fname):
     return pd.read_csv(fname)
 
 
+def get_forces(w1, w2, w3, w4, a=0.14, b=0.08):
+    f1 = (-w3 * a + w1 * (a + 2 * b)) / (2 * b)
+    f2 = (-w4 * a + w2 * (a + 2 * b)) / (2 * b)
+    f3 = (-w1 * a + w3 * (a + 2 * b)) / (2 * b)
+    f4 = (-w2 * a + w4 * (a + 2 * b)) / (2 * b)
+
+    print(f1, f2, f3, f4)
+    A = np.array([
+        [0, b, 2 * b, b],
+        [0, -b, 0, b],
+        [-b, 0, b, 0],
+        [b, b, 0, 2 * b]
+    ])
+    B = np.array([
+        [-a, b, 2 * b + a, b],
+        [0, -(a + b), 0, a + b],
+        [-(a + b), 0, a + b, 0],
+        [b, -a, b, 2 * b + a]
+    ])
+    W = np.array([
+        [w1],
+        [w2],
+        [w3],
+        [w4]
+    ])
+    # return np.linalg.solve(A, W)
+    print(W)
+    print(A)
+    print(np.linalg.det(A))
+    invA = np.linalg.inv(A)
+    print(invA)
+    F = invA.dot(B).dot(W)
+    return F
+
+
 if __name__ == '__main__':
     m_file = MeasurementWriter("write_test.csv")
     m_file.write(1, [2, 3, 4, 5], [6, 7, 8, 9])
     m_file.write(10, [11, 12, 13, 14], [15, 16, 17, 18])
+    print(get_forces(2000, 0, 1000, 0))
