6dd080faeff21bb30febcd0509658a95ccff0420
OlavHel <olav.lillegraven.helland@gmail.com>
2020-03-04 15:55:29 +0100

    On CurveRegression Change regression for F_ge into being a curve fit for the two models

--- Drone.py
+++ Drone.py
@@ -4,6 +4,7 @@ import numpy as np
 
 from Measurement import read_measurements
 from PolynomialRegression import PolynomialRegression
+from scipy.optimize import curve_fit
 
 
 class Drone:
@@ -19,6 +20,52 @@ class Drone:
         pass
 
 
+def model1(x, rho):
+    return 1 / (1 - rho * (1 / x) ** 2)
+
+
+def model2(x, a, b):
+    return a * np.exp(-x / b) + 1
+
+
+class F_geRegression:
+    def __init__(self, model):
+        if model == "model1":
+            self.param = [1]
+            self.covar = np.zeros(1)
+        elif model == "model2":
+            self.param = [1, 1]
+            self.covar = np.zeros((2, 2))
+        else:
+            print("No model chosen")
+
+        self.model = model
+
+    def predict(self, distances):
+        if self.model == "model1":
+            return model1(distances, self.param[0])
+        elif self.model == "model2":
+            return model2(distances, self.param[0], self.param[1])
+        else:
+            print("Missing model")
+            return []
+
+    def fit(self, x, y):
+        print(x.values, y.values)
+        if self.model == "model1":
+            solution = curve_fit(model1, x.values, y.values)
+        elif self.model == "model2":
+            solution = curve_fit(model2, x.values, y.values)
+        else:
+            print("Missing model")
+            return []
+
+        self.param = solution[0]
+        self.covar = solution[1]
+
+    def __str__(self):
+        return f"Model:{self.model}\nParameters:{self.param}"
+
 def find_f0(df: DataFrame):
     polynomial_regression = PolynomialRegression(3, bias=True)
     average_voltages = df.filter(regex="^voltage_\\d+$").mean(axis=1)
@@ -27,15 +74,15 @@ def find_f0(df: DataFrame):
     return polynomial_regression
 
 
-def find_fge(df: DataFrame, f0):
-    polynomial_regression = PolynomialRegression(3, bias=True)
+def find_fge(df: DataFrame, f0, model):
+    curve_regression = F_geRegression(model)
     average_voltages = df.filter(regex="^voltage_\\d+$").mean(axis=1)
     calc_weights = f0.predict(average_voltages.values)
     average_weights = df.filter(regex="^weight_\\d+$").mean(axis=1)
     error_weights = average_weights / calc_weights
     distances = df["distance"]
-    polynomial_regression.fit(distances.values, error_weights.values)
-    return polynomial_regression
+    curve_regression.fit(distances, error_weights)
+    return curve_regression
 
 
 def find_fu(df: DataFrame, f0, fge):
@@ -52,10 +99,18 @@ def find_fu(df: DataFrame, f0, fge):
 
 
 if __name__ == '__main__':
+    import matplotlib.pyplot as plt
+
     df = read_measurements("example_measurements.csv")
     f0 = find_f0(df[df["distance"] >= 20])
-    fge = find_fge(df, f0)
+    fge = find_fge(df, f0, "model1")
     fu = find_fu(df, f0, fge)
     print(f0)
     print(fge)
     print(fu)
+
+    plt.figure(1)
+    plt.scatter(df["voltage_1"], df['weight_1'])
+    plt.plot(np.arange(0,5,0.1), f0.predict(np.arange(0,5,0.1))*fge.predict(np.array([20])), color = "blue")
+    plt.plot(np.arange(0,5,0.1), f0.predict(np.arange(0,5,0.1)), color = "orange")
+    plt.show()
